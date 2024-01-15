a65fbd6391555911394ccf43a3a81b960ab5e34c
hubert-lopaciuk-vinci <hubert.lopaciuk@student.vinci.be>
2024-01-15 17:25:46 +0100

    fix: validations

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,5 +1,5 @@
 // TODO: Create tests suite for validation function
-const { isValid, validation8char, validationCharSpeciaux} = require('../validations');
+const { isValid, validation8char, validationCharSpeciaux, validationNumber} = require('../validations');
 
 
 describe('validations tests suites - isValid', () => {
@@ -30,6 +30,18 @@ describe('validations tests suites - validationCharSpeciaux', () => {
 
     test('should return true as gamertag has atleast 1 special character', () => {
         const result = validationCharSpeciaux("aaaaaaaaaa_");
+        expect(result).toBe(true);
+    });
+});
+
+describe('validations tests suites - validationNumber', () => {
+    test('should return false as gamertag doesnt have atleast 1 number', () => {
+        const result = validationNumber("aaaaaaaaaa_");
+        expect(result).toBe(false);
+    });
+
+    test('should return true as gamertag has atleast 1 number', () => {
+        const result = validationNumber("aaaaaaaaaa_1");
         expect(result).toBe(true);
     });
 });
