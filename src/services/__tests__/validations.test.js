0d1940eaf859719fd69ddf9c769e06c2e803a99e
hubert-lopaciuk-vinci <hubert.lopaciuk@student.vinci.be>
2024-01-15 17:31:07 +0100

    style: lint et prettier

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,6 +1,10 @@
 // TODO: Create tests suite for validation function
-const { isValid, validation8char, validationCharSpeciaux, validationNumber} = require('../validations');
-
+const {
+    isValid,
+    validation8char,
+    validationCharSpeciaux,
+    validationNumber,
+} = require('../validations');
 
 describe('validations tests suites - isValid', () => {
     test('should return true or false as the function is defined', () => {
@@ -11,37 +15,36 @@ describe('validations tests suites - isValid', () => {
 
 describe('validations tests suites - validation8char', () => {
     test('should return false as gamertag doesnt have atleast 8 characteres', () => {
-        const result = validation8char("aaa");
+        const result = validation8char('aaa');
         expect(result).toBe(false);
     });
 
     test('should return true as gamertag has atleast 8 characteres', () => {
-        const result = validation8char("aaaaaaaaaa");
+        const result = validation8char('aaaaaaaaaa');
         expect(result).toBe(true);
     });
 });
 
-
 describe('validations tests suites - validationCharSpeciaux', () => {
     test('should return false as gamertag doesnt have atleast 1 special character', () => {
-        const result = validationCharSpeciaux("aaaaaaaaa");
+        const result = validationCharSpeciaux('aaaaaaaaa');
         expect(result).toBe(false);
     });
 
     test('should return true as gamertag has atleast 1 special character', () => {
-        const result = validationCharSpeciaux("aaaaaaaaaa_");
+        const result = validationCharSpeciaux('aaaaaaaaaa_');
         expect(result).toBe(true);
     });
 });
 
 describe('validations tests suites - validationNumber', () => {
     test('should return false as gamertag doesnt have atleast 1 number', () => {
-        const result = validationNumber("aaaaaaaaaa_");
+        const result = validationNumber('aaaaaaaaaa_');
         expect(result).toBe(false);
     });
 
     test('should return true as gamertag has atleast 1 number', () => {
-        const result = validationNumber("aaaaaaaaaa_1");
+        const result = validationNumber('aaaaaaaaaa_1');
         expect(result).toBe(true);
     });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -2,22 +2,20 @@
 const isValid = () => Math.random() >= 0.5;
 
 const validation8char = (gamertag) => {
-    return (
-        gamertag.length >= 8
-    );
-}
+    return gamertag.length >= 8;
+};
 
 const validationCharSpeciaux = (gamertag) => {
     return /[&$!è§à_]/.test(gamertag);
-}
+};
 
 const validationNumber = (gamertag) => {
     return /[0-9]/.test(gamertag);
-}
+};
 
 module.exports = {
     isValid,
     validation8char,
     validationCharSpeciaux,
-    validationNumber
-}
+    validationNumber,
+};
