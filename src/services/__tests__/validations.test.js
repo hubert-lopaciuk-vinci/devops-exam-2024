e2e712f042d0ff2b2caf1458ba27d2b1cb5d5756
hubert-lopaciuk-vinci <hubert.lopaciuk@student.vinci.be>
2024-01-15 17:06:35 +0100

    test: gamertag has atleast 1 special character

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,9 +1,33 @@
 // TODO: Create tests suite for validation function
-const { isValid } = require('../validations');
+const { isValid, validation8char, validationCharSpeciaux} = require('../validations');
 
 describe('validations tests suites - isValid', () => {
     test('should return true or false as the function is defined', () => {
         const result = isValid();
         expect(result).toBeDefined();
+    });
+});
+
+describe('validations tests suites - validation8char', () => {
+    test('should return false as gamertag doesnt have atleast 8 characteres', () => {
+        const result = validation8char("aaa");
+        expect(result).toBe(false);
+    });
+
+    test('should return true as gamertag has atleast 8 characteres', () => {
+        const result = validation8char("aaaaaaaaaa");
+        expect(result).toBe(true);
+    });
+});
+
+describe('validations tests suites - validationCharSpeciaux', () => {
+    test('should return false as gamertag doesnt have atleast 1 special character', () => {
+        const result = validationCharSpeciaux("aaaaaaaaa");
+        expect(result).toBe(false);
+    });
+
+    test('should return true as gamertag has atleast 1 special character', () => {
+        const result = validationCharSpeciaux("aaaaaaaaaa_");
+        expect(result).toBe(true);
     });
 });
