aeab06087e4b336398c1917787d9990a0ab79958
hubert-lopaciuk-vinci <hubert.lopaciuk@student.vinci.be>
2024-01-15 17:03:44 +0100

    feat: gamertag has atleast 1 char speciaux

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,4 +1,18 @@
 // TODO: Refactor validation function to follow the defined validations rules
 const isValid = () => Math.random() >= 0.5;
 
-exports.isValid = isValid;
+const validation8char = (gamertag) => {
+    return (
+        gamertag.length >= 8
+    );
+}
+
+const validationCharSpeciaux = (gamertag) => {
+    return /[&$!è§à_]/.test(gamertag);
+}
+
+module.exports = {
+    isValid,
+    validation8char,
+    validationCharSpeciaux
+};
